0ddf29b3133f7eab883e1f33994716bae76f20bc
coldstar1993 <coldstar1993@163.com>
2023-07-25 22:31:59 +0800

    add WithdrawInfoDtoSchema

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,10 +1,12 @@
 export { L2TxDTO } from "./l2-tx-dto";
+export { WithdrawInfoDTO } from './withdraw-info-dto'
 export { MerklePathDTO, MerkleProofReqParam } from "./merkle-path";
 export { Tree } from './constant';
 
 import L2TxDTOSchema from './l2-tx-dto-schema.json' assert { type: "json" };
 import MerklePathDTOSchema from './MerklePathDTO-schema.json' assert { type: "json" };
 import MerkleProofReqParamSchema from './MerkleProofReqParam-schema.json' assert { type: "json" };
+import WithdrawInfoDtoSchema from './withdraw-info-dto-schema.json' assert { type: "json" };
 
-export { L2TxDTOSchema, MerklePathDTOSchema, MerkleProofReqParamSchema };
+export { L2TxDTOSchema, MerklePathDTOSchema, MerkleProofReqParamSchema, WithdrawInfoDtoSchema };
 
---
+++ packages/types/src/withdraw-info-dto.ts
@@ -0,0 +1,18 @@
+export interface WithdrawInfoDTO {
+    id: number;
+    /**
+     * hash of corresponding L2 tx
+     */
+    l2TxHash: string;
+    secret: string;
+    ownerPk: string;
+    accountRequired: string;
+    /**
+     * could be optional
+     */
+    creatorPk: string;
+    value: string;
+    assetId: string;
+    inputNullifier: string;
+    noteType: string;
+}
