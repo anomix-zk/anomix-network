789f55ecfee3cb18583f29c7913821181d41d029
coldstar1993 <coldstar1993@163.com>
2023-07-25 19:59:33 +0800

    improve config

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,9 +2,9 @@ export { L2TxDTO } from "./l2-tx-dto";
 export { MerklePathDTO, MerkleProofReqParam } from "./merkle-path";
 export { Tree } from './constant';
 
-import L2TxDTOSchema from './l2-tx-dto-schema.json';
-import MerklePathDTOSchema from './MerklePathDTO-schema.json';
-import MerkleProofReqParamSchema from './MerkleProofReqParam-schema.json';
+import L2TxDTOSchema from './l2-tx-dto-schema.json' assert { type: "json" };
+import MerklePathDTOSchema from './MerklePathDTO-schema.json' assert { type: "json" };
+import MerkleProofReqParamSchema from './MerkleProofReqParam-schema.json' assert { type: "json" };
 
 export { L2TxDTOSchema, MerklePathDTOSchema, MerkleProofReqParamSchema };
 
