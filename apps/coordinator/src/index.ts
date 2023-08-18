3fd4e9f2b0e0ea1c968facac67a4b868a10ac3de
coldstar1993 <coldstar1993@163.com>
2023-08-19 00:45:47 +0800

    improve the starter: index.ts

--- apps/coordinator/src/index.ts
+++ apps/coordinator/src/index.ts
@@ -1,12 +1,70 @@
+import { Worker } from "worker_threads";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import { FastifyCore } from './app'
-import { initORM } from './lib/orm'
+import { activeMinaInstance } from "@anomix/utils";
 
-const server = async () => {
-    await initORM()
-    const app = new FastifyCore()
-    await app.listen()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const workerMap = new Map<string, Worker>();
+
+function bootTaskTracerThread() {
+    // init worker thread A
+    const worker = new Worker(`${__dirname}/task-tracer.js`);// TODO
+    worker.on('online', () => {
+        console.log('task-tracer is online...');
+    })
+
+    worker.on('exit', (exitCode: number) => {
+        // create a new worker for http-server
+        bootTaskTracerThread();
+    })
+
+    workerMap.set('TaskTracer', worker);
 }
 
-export default server
+function bootMempoolWatcherThread() {
+    // init worker thread A
+    const worker = new Worker(`${__dirname}/mempool-watcher.js`);// TODO
+    worker.on('online', () => {
+        console.log('mempool-watcher is online...');
+    })
 
-server()
+    worker.on('exit', (exitCode: number) => {
+        // create a new worker for http-server
+        bootMempoolWatcherThread();
+    })
+
+    workerMap.set('MempoolWatcher', worker);
+}
+
+function bootProofTriggerThread() {
+    // init worker thread A
+    const worker = new Worker(`${__dirname}/proof-trigger.js`);// TODO
+    worker.on('online', () => {
+        console.log('proof-trigger is online...');
+    })
+
+    worker.on('exit', (exitCode: number) => {
+        // create a new worker for http-server
+        bootProofTriggerThread();
+    })
+
+    workerMap.set('ProofTrigger', worker);
+}
+
+// init Mina tool
+await activeMinaInstance();// TODO improve it to configure graphyQL endpoint
+
+bootTaskTracerThread();
+
+bootProofTriggerThread();
+
+bootMempoolWatcherThread();
+
+// init Mina tool
+await activeMinaInstance();// TODO improve it to configure graphyQL endpoint
+
+const app = new FastifyCore();
+await app.listen();
