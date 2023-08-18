6d26a59ec6d072ba935c115d9e16c83335b31074
coldstar1993 <coldstar1993@163.com>
2023-08-19 00:32:38 +0800

    mv query-could-stop to rollup dir

--- apps/deposit-processor/src/app/routes/rollup/index.ts
+++ apps/deposit-processor/src/app/routes/rollup/index.ts
@@ -1,11 +1,13 @@
 
 import { FastifyPlugin } from "fastify"
 import { triggerRollupBatch } from "./trigger-rollup-batch";
+import { queryCouldStopRollup } from "./query-could-stop-rollup";
 
 export const txEndpoint: FastifyPlugin = async (
     instance,
     options,
     done
 ): Promise<void> => {
+    instance.register(queryCouldStopRollup);
     instance.register(triggerRollupBatch);
 }
