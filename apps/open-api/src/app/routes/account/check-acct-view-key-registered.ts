1c65927b3c2f188193d689c33ed3ba75ff6141a6
coldstar1993 <coldstar1993@163.com>
2023-08-20 20:21:59 +0800

    add checkAcctViewKeyRegistered

---
+++ apps/open-api/src/app/routes/account/check-acct-view-key-registered.ts
@@ -0,0 +1,96 @@
+// @Get('/account/acctvk/:aliashash')
+/**
+ * (2)供client根据account_viewing_key查询recipient的alias_nullifier
+ */
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { getConnection } from 'typeorm';
+
+import { RequestHandler } from '@/lib/types'
+import { Account, MemPlL2Tx } from '@anomix/dao'
+import { BaseResponse, L2TxStatus } from "@anomix/types";
+
+export const checkAcctViewKeyRegistered: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "GET",
+        url: "/account/check-acct-view-key-registered",
+        //preHandler: [instance.authGuard],
+        schema,
+        handler
+    })
+}
+
+type reqBody = { acctViewKey: string, includePending: boolean };
+
+export const handler: RequestHandler<reqBody, null> = async function (
+    req,
+    res
+): Promise<BaseResponse<boolean>> {
+    const { acctViewKey, includePending } = req.body
+
+    const connection = getConnection();
+    const accountRepository = connection.getRepository(Account)
+    try {
+        let code = 0;// registered!
+        let data = true;
+        let msg = '';
+        const account = await accountRepository.findOne({ where: { acctPk: acctViewKey } });
+        if (account /* && includePending */) {
+            const mpL2TxRepo = connection.getRepository(MemPlL2Tx);
+            const status = (await mpL2TxRepo.findOne(account.l2TxId))!.status;
+            if (status == L2TxStatus.FAILED) {
+                code = 1;// not registered!
+                data = false;
+            } else if (status == L2TxStatus.PENDING) {
+                msg = 'pending';
+            }
+        }
+
+        return {
+            code,
+            data,
+            msg
+        };
+
+    } catch (err) {
+        console.log(err);
+
+        throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+
+}
+const schema = {
+    description: 'check if acctViewKey has been registered',
+    tags: ["Account"],
+    body: {
+        type: "object",
+        properties: {
+            acctViewKey: {
+                type: "string",
+            },
+            includePending: {
+                type: "boolean"
+            }
+        }
+    },
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: "boolean"
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
