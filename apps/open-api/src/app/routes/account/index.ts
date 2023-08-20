499935b4766bd71df5c6fd5d26a2a06c29d8f318
coldstar1993 <coldstar1993@163.com>
2023-08-20 21:11:27 +0800

    expose endpoint

--- apps/open-api/src/app/routes/account/index.ts
+++ apps/open-api/src/app/routes/account/index.ts
@@ -9,12 +9,14 @@ import { queryAcctViewKeyByAlias } from "./query-acct-view-key-by-alias";
 import { queryAliasByAcctViewKey } from "./query-alias-by-acct-view-key";
 import { checkAcctViewKeyRegistered } from "./check-acct-view-key-registered";
 import { checkAliasRegister } from "./check-alias-registered";
+import { checkAliasAlignWithViewKey } from "./check-alias-align-with-acct-view-key";
 
 export const accountEndpoint: FastifyPlugin = async (
     instance,
     options,
     done
 ): Promise<void> => {
+    instance.register(checkAliasAlignWithViewKey);
     instance.register(checkAcctViewKeyRegistered);
     instance.register(checkAliasRegister);
     instance.register(queryAcctViewKeyByAlias);
