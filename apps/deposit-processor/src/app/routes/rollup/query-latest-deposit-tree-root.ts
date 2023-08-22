9604fcabff4f70b9be5ec8a9663ce2beeaa1c7dd
coldstar1993 <coldstar1993@163.com>
2023-08-22 13:16:32 +0800

    add queryLatestDepositTreeRoot

---
+++ apps/deposit-processor/src/app/routes/rollup/query-latest-deposit-tree-root.ts
@@ -0,0 +1,69 @@
+
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { RequestHandler } from '@/lib/types'
+import { BaseResponse, MerkleTreeId } from "@anomix/types";
+import { type } from "os";
+
+/**
+ * query deposit-tree root
+ */
+export const queryLatestDepositTreeRoot: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "GET",
+        url: "/rollup/deposit-tree-root",
+        //preHandler: [instance.authGuard],
+        schema,
+        handler
+    })
+}
+
+export const handler: RequestHandler<null, null> = async function (
+    req,
+    res
+): Promise<BaseResponse<string>> {
+    try {
+        /* single thread for both http-server and deposit_rollup, so no need to check if WorldState has an onging Flow  
+        // check if WorldState has an onging Flow     
+        if (this.worldState.ongingFlow) {
+            return {
+                code: 1, data: undefined, msg: ''
+            };
+        } 
+        */
+
+        // if deposit_processor just send out a deposit-rollup L1tx that is not yet confirmed at Mina Chain, then return the latest DEPOSIT_TREE root !!
+        // later if the seq-rollup L1Tx of the L2Block with this root is confirmed before this deposit-rollup L1tx (even though this case is rare), then the seq-rollup L1Tx fails!!
+        const latestDepositTreeRoot = this.worldState.worldStateDB.getRoot(MerkleTreeId.DEPOSIT_TREE, true).toString();
+        return {
+            code: 0, data: latestDepositTreeRoot, msg: ''
+        };
+    } catch (err) {
+        throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
+
+const schema = {
+    description: 'query deposit-tree root',
+    tags: ['Rollup'],
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: 'string'
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
