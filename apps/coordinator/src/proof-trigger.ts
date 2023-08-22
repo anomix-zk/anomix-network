a99cf21c80472922069e47119d0affac09938020
coldstar1993 <coldstar1993@163.com>
2023-08-22 13:08:09 +0800

    only all blocks has depositTx are confirmed,

--- apps/coordinator/src/proof-trigger.ts
+++ apps/coordinator/src/proof-trigger.ts
@@ -1,4 +1,3 @@
-
 import { $axiosDeposit, $axiosSeq } from "./lib/api";
 import { getConnection, In } from 'typeorm';
 import { Block, DepositProverOutput, DepositTreeTrans, L2Tx, SeqStatus } from '@anomix/dao';
@@ -9,28 +8,65 @@ const periodRange = 5 * 60 * 1000
 
 setInterval(proofTrigger, periodRange); // exec/5mins
 
+/**
+ * each time seq-rollup-proof start: if there are blocks containing depositL2Tx, {Need stop Broadcasting DepositRollupProof as L1Tx}, else {select the lowest-id depositTreeTrans with PROVED status, and broadcast its proof}
+ * * this is the uniform point to decide if could Broadcasting DepositRollupProof as L1Tx
+ */
 async function proofTrigger() {
     try {
-        // if any one of L2Block that are about to rolling up contains depositL2Tx, then must stop Broadcasting DepositRollupProof as L1Tx !!
-        let couldBroadcastDepositRollupProof = true;
-
         const connection = getConnection();
         const blockRepo = connection.getRepository(Block);
         const blockList = await blockRepo.find(
             {
                 where: { status: In([BlockStatus.PENDING, BlockStatus.PROVED]) },
                 order: { id: 'ASC' }
+            });// blockList might contains blocks at previous trigger-round.
+
+        // if any one of L2Block that are about to rolling up contains depositL2Tx, then must stop Broadcasting DepositRollupProof as L1Tx !!
+        const couldTriggerDepositContact = !(blockList!.some(block => {
+            // blockList might contains blocks at previous trigger-round,
+            // which means that if blocks at previous trigger-round has not yet complete, then need stop Broadcasting DepositRollupProof as L1Tx,
+            // further means only all existing blocks containing DepositL2Tx has confirmed at Layer1, then deposit-processor could broadcast DepositRollupProof as L1Tx
+
+            return block.depositCount != '0'//ie. has depositL2Tx within this trigger-round
+        }))
+
+        if (couldTriggerDepositContact) {// if there is no depositTx inside the blockList above, then could Broadcast DepositRollupProof as L1Tx !!
+            // update db status to SeqStatus.NotAtRollup
+            await setSeqProofStatus(SequencerStatus.NotAtRollup);
+
+            // obtain the first DepositTreeTrans and trigger deposit-contract-call
+            const connection = getConnection();
+            const transRepo = connection.getRepository(DepositTreeTrans);
+            const dTran = await transRepo.findOne({// one is enough among the period range(5mins).
+                where: {
+                    status: In([DepositTreeTransStatus.PROVED])
+                },
+                order: { id: 'ASC' },
             });
 
-        blockList!.forEach(async (block, indx) => {
-            if (block.depositCount != '0') {//ie. has depositL2Tx
-                if (couldBroadcastDepositRollupProof) {// this could avoid double set status, when multiple blocks has depositL2tx.
-                    couldBroadcastDepositRollupProof = false;
-                    // update db status to SeqStatus.ATROLLUP
-                    await setSeqProofStatus(SequencerStatus.AtRollup);
-                }
+            if (!dTran) {
+                return;
             }
 
+            // dTran MUST align with the AnomixEntryContract's states.
+            // assert
+            // assert
+            //
+            //
+
+            await $axiosDeposit.post<BaseResponse<string>>(`/rollup/contract-call/${dTran.id}`).then(r => {
+                if (r.data.code == 1) {
+                    throw new Error(r.data.msg);
+                }
+            });// TODO future: could improve when fail by 'timeout' after retry
+
+        } else {//ie. has depositL2Tx
+            // update db status to SeqStatus.ATROLLUP
+            await setSeqProofStatus(SequencerStatus.AtRollup);
+        }
+
+        blockList!.forEach(async (block, indx) => {
             if (indx == 0 && block!.status == BlockStatus.PROVED) {// the lowest one with PROVED status, must align with the onchain state inside AnomixRollupContract.
                 const rollupTaskDto = {
                     taskType: RollupTaskType.ROLLUP_CONTRACT_CALL,
@@ -95,43 +131,9 @@ async function proofTrigger() {
             }
         });
 
-        // if there is no depositTx inside the blockList above, then could Broadcast DepositRollupProof as L1Tx !!
-        if (couldBroadcastDepositRollupProof) {
-            // update db status to SeqStatus.NotAtRollup
-            await setSeqProofStatus(SequencerStatus.NotAtRollup);
-
-            // obtain the first DepositTreeTrans and trigger deposit-contract-call
-            const connection = getConnection();
-            const transRepo = connection.getRepository(DepositTreeTrans);
-            const dTran = await transRepo.findOne({// one is enough among the period range(5mins).
-                where: {
-                    status: In([DepositTreeTransStatus.PROVED])
-                },
-                order: { id: 'ASC' },
-            });
-
-            if (!dTran) {
-                return;
-            }
-
-            // dTran MUST align with the AnomixEntryContract's states.
-            // assert
-            // assert
-            //
-            //
-
-            await $axiosDeposit.post<BaseResponse<string>>(`/rollup/contract-call/${dTran.id}`).then(r => {
-                if (r.data.code == 1) {
-                    throw new Error(r.data.msg);
-                }
-            });// TODO future: could improve when fail by 'timeout' after retry
-        }
-
-    }
-
     } catch (error) {
-    console.error(error);
-}
+        console.error(error);
+    }
 }
 
 async function setSeqProofStatus(s: SequencerStatus) {
