b171956d72b472f65627d286805a51af8c35da5f
coldstar1993 <coldstar1993@163.com>
2023-08-31 23:07:21 +0800

    imporve request interceptor

--- apps/deposit-processor/src/lib/api/interceptors.ts
+++ apps/deposit-processor/src/lib/api/interceptors.ts
@@ -16,9 +16,12 @@ const callbackTrigger: CallbackTrigger = {
     responseError: (null as any) as ResponseErrorCallback
 };
 
-$axiosProofGenerator.interceptors.request.use((value: AxiosRequestConfig) => {
-    //
-    console.log(JSON.stringify(value));
+$axiosProofGenerator.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
 })
 
 $axiosProofGenerator.interceptors.response.use(
