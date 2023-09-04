1ac2df56bbd0c73e0e6c64a7827ccda1a275e304
coldstar1993 <coldstar1993@163.com>
2023-09-04 13:21:03 +0800

    prefill dataTreeRoot into indexDB

--- apps/rollup-sequencer/src/web-server.ts
+++ apps/rollup-sequencer/src/web-server.ts
@@ -4,18 +4,10 @@ import config from "@/lib/config";
 import { activeMinaInstance } from "@anomix/utils";
 import { WithdrawDB } from './worldstate/withdraw-db';
 import fs from "fs";
+import { MerkleTreeId } from '@anomix/types';
 
 // init Mina tool
 await activeMinaInstance();// TODO improve it to configure graphyQL endpoint
-
-
-const existDB = fs.existsSync(config.worldStateDBPath);
-const worldStateDB = new WorldStateDB(config.worldStateDBPath);
-if (!existDB) {
-    await worldStateDB.initTrees();
-} else {
-    await worldStateDB.loadTrees();
-}
 
 // init mysqlDB
 const rollupDB = new RollupDB();
@@ -23,6 +15,22 @@ rollupDB.start();
 
 // init IndexDB
 const indexDB = new IndexDB(config.indexedDBPath);
+
+const existDB = fs.existsSync(config.worldStateDBPath);
+const worldStateDB = new WorldStateDB(config.worldStateDBPath);
+if (!existDB) {
+    await worldStateDB.initTrees();
+
+    // prepare data_tree root into indexDB & dataTreeRootsTree
+    const dataTreeRoot = worldStateDB.getRoot(MerkleTreeId.DATA_TREE, false);
+    const index = worldStateDB.getNumLeaves(MerkleTreeId.DATA_TREE_ROOTS_TREE, false);// 0n
+    await worldStateDB.appendLeaves(MerkleTreeId.DATA_TREE_ROOTS_TREE, [dataTreeRoot]);
+    await indexDB.put(`${MerkleTreeId[MerkleTreeId.DATA_TREE_ROOTS_TREE]}:${dataTreeRoot.toString()}`, `${index}`);// '0'
+
+} else {
+    await worldStateDB.loadTrees();
+
+}
 
 // construct WorldState
 const worldState = new WorldState(worldStateDB, rollupDB, indexDB);
