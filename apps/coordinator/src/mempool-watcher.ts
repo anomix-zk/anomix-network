fd6de7b5d6c6a49424cf7c9bdfbaa9356db8c821
coldstar1993 <coldstar1993@163.com>
2023-09-09 19:40:50 +0800

    improve more logs

--- apps/coordinator/src/mempool-watcher.ts
+++ apps/coordinator/src/mempool-watcher.ts
@@ -49,6 +49,9 @@ parentPort?.postMessage({// if it's a subThread
 setInterval(mempoolWatch, 20 * 60 * 1000);// exec/1min
 
 async function mempoolWatch() {
+
+    logger.info('start a new round...');
+
     try {
         const rollupTaskDto = {
             taskType: RollupTaskType.SEQUENCE,
@@ -58,6 +61,7 @@ async function mempoolWatch() {
 
         // when there exists a highFee L2Tx, then trigger seq directly
         if (highFeeTxExit) {
+            logger.info('highFeeTxExit == true, trigger /rollup/seq-trigger...');
             // trigger seq
             await $axiosSeq.post<BaseResponse<string>>('/rollup/seq-trigger', rollupTaskDto).then(r => {
                 if (r.data.code == 1) {
@@ -70,6 +74,8 @@ async function mempoolWatch() {
         }
 
         if ((new Date().getTime() - lastSeqTs) < 1 * 60 * 1000) {// if interval event just happens sooner after highFeeTx exits
+            logger.info('interval time is not enough, stop.');
+
             return;
         }
 
@@ -84,6 +90,8 @@ async function mempoolWatch() {
             if (!couldSeq) {
                 // 1) check maxMpTxCnt
                 if (mpTxList.length >= config.maxMpTxCnt) {
+                    logger.info('mpTxList.length is greater than config.maxMpTxCnt.');
+
                     couldSeq = true;
                 }
             }
@@ -94,6 +102,8 @@ async function mempoolWatch() {
                     return p + Number(c.txFee);
                 }, 0);
                 if (txFeeSum >= config.maxMpTxFeeSUM) {
+                    logger.info('txFeeSum in memory pool is greater than config.maxMpTxFeeSUM.');
+
                     couldSeq = true;
                 }
             }
@@ -111,6 +121,8 @@ async function mempoolWatch() {
                     return;
                 }
                 if (new Date().getTime() - block!.createdAt.getTime() >= config.maxBlockInterval) {
+                    logger.info('latest block.createdAt is greater than config.maxBlockInterval.');
+
                     couldSeq = true;
                 }
             }
@@ -129,6 +141,8 @@ async function mempoolWatch() {
                         needStopDepositProcessor = blocks.some(b => {
                             return b.depositCount > 0
                         });
+
+                        logger.info('blocks have depositTx, set needStopDepositProcessor = ', needStopDepositProcessor);
                     });
                 }
 
@@ -138,15 +152,20 @@ async function mempoolWatch() {
                  */
                 const depositProcessorSignal = (await queryRunner.manager.findOne(DepositProcessorSignal, { where: { id: 1 } }))!;
                 if (needStopDepositProcessor) {
+                    logger.info('stop depositProcessor to call AnomixEntryContract...');
+
                     // stop depositProcessor to call AnomixEntryContract
                     depositProcessorSignal.signal = DepositProcessingSignal.CAN_NOT_TRIGGER_CONTRACT;
                     await queryRunner.manager.save(depositProcessorSignal);
 
                 } else {
+                    logger.info('re-allow depositProcessor to call AnomixEntryContract...');
+
                     // allow depositProcessor to call AnomixEntryContract 
                     depositProcessorSignal.signal = DepositProcessingSignal.CAN_TRIGGER_CONTRACT;
                     await queryRunner.manager.save(depositProcessorSignal); // save before http-call below
 
+                    logger.info('obtain the first DepositTreeTrans...');
                     // obtain the first DepositTreeTrans and trigger deposit-contract-call
                     const depositTreeTrans = await queryRunner.manager.findOne(DepositTreeTrans, {// one is enough among the period range(5mins).
                         where: {
@@ -155,11 +174,13 @@ async function mempoolWatch() {
                         order: { id: 'ASC' },
                     });
 
+                    logger.info('obtain the first DepositTreeTrans=', depositTreeTrans?.id);
                     if (depositTreeTrans) {
                         // dTran MUST align with the AnomixEntryContract's states.
                         // assert
                         // assert
 
+                        logger.info('trigger /rollup/contract-call/', depositTreeTrans?.id);
                         await $axiosDeposit.post<BaseResponse<string>>(`/rollup/contract-call/${depositTreeTrans.id}`).then(r => {
                             if (r.data.code == 1) {
                                 throw new Error(r.data.msg);
@@ -169,6 +190,7 @@ async function mempoolWatch() {
                 }
 
                 // trigger seq
+                logger.info('trigger /rollup/seq-trigger...');
                 await $axiosSeq.post<BaseResponse<string>>('/rollup/seq-trigger', rollupTaskDto).then(r => {
                     if (r.data.code == 1) {
                         logger.error(r.data.msg);
@@ -176,6 +198,8 @@ async function mempoolWatch() {
                     }
                 });
             }
+
+            logger.info('this round is done.');
 
             await queryRunner.commitTransaction();
         } catch (error) {
