84aeee7f7efe3d9b7bdd09b7a34bb7719cdd7756
coldstar1993 <coldstar1993@163.com>
2023-09-23 20:54:24 +0800

    comment test data

--- apps/rollup-sequencer/src/worldstate/index-db.ts
+++ apps/rollup-sequencer/src/worldstate/index-db.ts
@@ -15,6 +15,21 @@ export class IndexDB {
     private readonly indexDB: LevelUp
     constructor(dbPath: string) {
         this.indexDB = levelup(leveldown(dbPath));
+        /*
+                this.indexDB.put('DATA_TREE:21341325558200709901033723790493204378145354968718845965994415398766766623987', '4');
+                this.indexDB.put('DATA_TREE:4213916911589870202915817812879850334681168861484460095192137507642676201952', '5');
+                this.indexDB.put('DATA_TREE:10211729120694068663433900697109900289072398976506989656663663089480401482475', '6');
+        
+                this.indexDB.put('NULLIFIER_TREE:12470141285728746039246699311556353222834592649603163512740848178120721474643', '1');
+                this.indexDB.put('NULLIFIER_TREE:12762135127876208705654756334482501197156915425313803431609397567607989124934', '2');
+        
+                this.indexDB.put('DATA_TREE_ROOTS_TREE:1290841069417130398496203009517035270808855829893677337248047547477045753664', '2');
+        
+
+        this.indexDB.createReadStream().on('data', function (entry) {
+            console.log(`${entry.key}: ${entry.value}`);
+        });
+        */
     }
 
     async get(key: string) {
