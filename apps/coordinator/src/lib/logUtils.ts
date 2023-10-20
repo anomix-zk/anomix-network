6db5a5bccab1326ada183b49e6bbf0bf5c27f19f
coldstar1993 <coldstar1993@163.com>
2023-10-20 10:56:16 +0800

    improve logUtil

--- apps/coordinator/src/lib/logUtils.ts
+++ apps/coordinator/src/lib/logUtils.ts
@@ -1,30 +1,35 @@
 import pino, { Logger } from 'pino';
 import config from './config';
 
-const logLevelData = { '*': 'info' };
-const logLevels = new Map<string, string>(Object.entries(logLevelData));
+const loggerFlag = process.argv[2] ?? 'Main';
 
-function getLogLevel(logger: string): string {
-    return logLevels.get(logger) || logLevels.get('*') || 'info';
-}
+const pinoObj = pino({
+    name: loggerFlag,
+    timestamp: pino.stdTimeFunctions.isoTime,
+    transport: {
+        targets: [
+            {
+                target: 'pino/file',
+                level: 'info',
+                options: { destination: config.pinoLogFilePath.concat(`/coordinator-${loggerFlag}.log`) }
+            },
+            { target: 'pino-pretty', level: 'info', options: { destination: '/dev/stdout' } }
+        ]
+    }
+});
+
+const childLoggerMap = new Map<string, Logger>();
 
 // TODO need configure file storage, pattern, etc.
-export function getLogger(name: string, logFileName?: string): Logger {
-    return pino({
-        name,
-        level: getLogLevel(name),
-        timestamp: pino.stdTimeFunctions.isoTime,
-        transport: {
-            targets: [
-                {
-                    target: 'pino/file',
-                    level: 'info',
-                    options: { destination: config.pinoLogFilePath.concat('/coordinator-log' + (logFileName ? ('-' + logFileName) : '') + '.log') }
-                },
-                { target: 'pino-pretty', level: 'info', options: { destination: '/dev/stdout' } }
-            ]
-        }
-    });
+export function getLogger(childSegment: string): Logger {
+    if (!childSegment) {
+        return pinoObj;
+    }
+    if (!childLoggerMap.get(childSegment)) {
+        childLoggerMap.set(childSegment, pinoObj.child({ segment: childSegment }));
+    }
+
+    return childLoggerMap.get(childSegment)!;
 }
 
 
