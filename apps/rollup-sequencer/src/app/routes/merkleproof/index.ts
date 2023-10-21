ba820652e1c98722e4f3cec78a098c5739f554aa
coldstar1993 <coldstar1993@163.com>
2023-10-21 11:02:24 +0800

    add one more endpoint for sync data tree

--- apps/rollup-sequencer/src/app/routes/merkleproof/index.ts
+++ apps/rollup-sequencer/src/app/routes/merkleproof/index.ts
@@ -2,6 +2,10 @@ import { FastifyPlugin } from "fastify"
 import { queryMerkleTreeInfo } from "./query-merkle-tree-info";
 import { queryMerkleProof } from "./query-merkle-proof";
 import { queryWitnessByWithdrawNotes } from "./query-witness-by-withdraw-notes";
+import { syncLazyDataTreeByBlockId } from "./sync-lazy-data-tree-by-blockId";
+import { queryUserTreeInfo } from "./query-user-tree-info";
+import { appendUserNullifierTreeByHand } from "./append-user-tree-leaves";
+import { appendTreeByHand } from "./append-tree-leaves";
 import { syncLazyDataTree } from "./sync-lazy-data-tree";
 //import { queryWorldStateworldState } from './query-worldstate-state'
 //import { checkCommitmentsExist } from './check-commiments-exist';
@@ -17,6 +21,10 @@ export const merkleProofEndpoint: FastifyPlugin = async (
     instance.register(queryMerkleProof);
     instance.register(queryWitnessByWithdrawNotes);
     instance.register(syncLazyDataTree);
+    instance.register(syncLazyDataTreeByBlockId);
+    instance.register(queryUserTreeInfo);
+    instance.register(appendUserNullifierTreeByHand);
+    instance.register(appendTreeByHand);
 
     //instance.register(rollupSeqTrigger);
     // instance.register(checkCommitmentsExist);
--- apps/rollup-sequencer/src/app/routes/merkleproof/sync-lazy-data-tree.ts
+++ apps/rollup-sequencer/src/app/routes/merkleproof/sync-lazy-data-tree.ts
@@ -1,15 +1,17 @@
 
 import httpCodes from "@inip/http-codes"
 import { FastifyPlugin } from "fastify"
-import { BaseResponse, BlockCacheType, MerkleTreeId } from '@anomix/types'
+import { BaseResponse, BlockCacheStatus, BlockCacheType, MerkleTreeId } from '@anomix/types'
 import { RequestHandler } from '@/lib/types'
 import { getConnection } from "typeorm"
 import { Block, BlockCache } from "@anomix/dao"
 import { Field } from "o1js";
+import { getLogger } from "@/lib/logUtils"
 
+const logger = getLogger('syncLazyDataTree');
 
 /**
- * query MerkleTree Info
+ * sync Lazy Data Tree
  */
 export const syncLazyDataTree: FastifyPlugin = async function (
     instance,
@@ -18,67 +20,99 @@ export const syncLazyDataTree: FastifyPlugin = async function (
 ): Promise<void> {
     instance.route({
         method: "GET",
-        url: "/merkletree/sync/:blockId",
+        url: "/merkletree/sync-data-tree",
         //preHandler: [instance.authGuard],
         schema,
         handler
     })
 }
 
-export const handler: RequestHandler<null, { blockId: number }> = async function (
+export const handler: RequestHandler<null, null> = async function (
     req,
     res
 ): Promise<BaseResponse<string>> {
-    const blockId = req.params.blockId;
+    const connection = getConnection();
+    const queryRunner = connection.createQueryRunner();
 
+    await queryRunner.startTransaction();
     try {
-
-        const blockRepo = getConnection().getRepository(Block);
-        const block = (await blockRepo.findOne({ where: { id: blockId } }));
-
-        if (!block) {
+        const blockCacheList = await queryRunner.manager.find(BlockCache, {
+            where: { type: BlockCacheType.DATA_TREE_UPDATES, status: BlockCacheStatus.CONFIRMED }, order: { blockId: 'ASC' }
+        });
+        if (blockCacheList.length == 0) {
+            logger.info('no blockCaches for DATA_TREE_UPDATES');
             return {
-                code: 1, data: '', msg: 'non-exiting blockId'
+                code: 0, data: '', msg: 'no blockCaches for DATA_TREE_UPDATES'
             };
         }
 
-        // check if sync_date_tree root is aligned with 
-        // check if duplicated call
-        const syncDataTreeRoot = this.worldState.worldStateDB.getRoot(MerkleTreeId.SYNC_DATA_TREE, false);
-        if (block.dataTreeRoot0 == syncDataTreeRoot.toString()) {
-            const blockCacheRepo = getConnection().getRepository(BlockCache);
-            const cachedStr = (await blockCacheRepo.findOne({ where: { blockId, type: BlockCacheType.DATA_TREE_UPDATES } }))!.cache;
+        logger.info(`start syncing data_tree.`);
 
-            const blockCachedUpdates1 = (JSON.parse(cachedStr) as Array<string>).map(i => Field(i));
-            await this.worldState.worldStateDB.appendLeaves(MerkleTreeId.SYNC_DATA_TREE, blockCachedUpdates1);
+        logger.info(`blockCacheList: ${blockCacheList.map(bc => { return { id: bc.id, blockId: bc.blockId } })}`);
 
-            await this.worldState.worldStateDB.commit(); // here only 'SYNC_DATA_TREE' commits underlyingly           
+        for (let index = 0; index < blockCacheList.length; index++) {
+            const blockCache = blockCacheList[index];
+            logger.info(`processing blockCache.id = ${blockCache.id} ...`);
 
-            return {
-                code: 0, data: '', msg: ''
-            };
-        } else {
-            return {
-                code: 1, data: '', msg: 'rejected duplicated call!'
-            };
+            const block = await queryRunner.manager.findOne(Block, { id: blockCache.blockId });
+            logger.info(`block.id: ${block!.id}, block!.dataTreeRoot0: ${block!.dataTreeRoot0}`);
+
+            let syncDataTreeRoot = this.worldState.worldStateDB.getRoot(MerkleTreeId.SYNC_DATA_TREE, true);
+            logger.info(`before sync, syncDataTreeRoot: ${syncDataTreeRoot.toString()}`);
+            let syncDataTreeLeafNum = this.worldState.worldStateDB.getNumLeaves(MerkleTreeId.SYNC_DATA_TREE, true);
+            logger.info(`before sync, syncDataTreeLeafNum: ${syncDataTreeLeafNum.toString()}`);
+
+            if (block!.dataTreeRoot0 != syncDataTreeRoot.toString()) {
+                logger.error(`block.dataTreeRoot0 is not aligned with current syncDataTreeRoot`);
+                throw new Error(`block.dataTreeRoot0 is not aligned with current syncDataTreeRoot`);
+            }
+
+            logger.info(`blockCache.cache: ${blockCache.cache}`);
+            const commitments = (JSON.parse(blockCache.cache) as string[]).map(c => Field(c));
+            logger.info(`cooresponding commitments: ${commitments.toString()}`);
+            await this.worldState.worldStateDB.appendLeaves(MerkleTreeId.SYNC_DATA_TREE, commitments);
+
+            syncDataTreeRoot = this.worldState.worldStateDB.getRoot(MerkleTreeId.SYNC_DATA_TREE, true);
+            logger.info(`after sync, syncDataTreeRoot: ${syncDataTreeRoot.toString()}`);
+            syncDataTreeLeafNum = this.worldState.worldStateDB.getNumLeaves(MerkleTreeId.SYNC_DATA_TREE, true);
+            logger.info(`after sync, syncDataTreeLeafNum: ${syncDataTreeLeafNum.toString()}`);
+
+            if (block!.dataTreeRoot1 != syncDataTreeRoot.toString()) {
+                logger.info(`block!.dataTreeRoot1: ${block!.dataTreeRoot1}`);
+
+                logger.error(`after sync blockCache, block.dataTreeRoot1 is not aligned with current syncDataTreeRoot`);
+                throw new Error(`after sync blockCache, block.dataTreeRoot1 is not aligned with current syncDataTreeRoot`);
+            }
+
+            logger.info(`process blockCache.id = ${blockCache.id} done.`);
+
+            blockCache.status = BlockCacheStatus.PROCESSED;
         }
+
+        await queryRunner.manager.save(blockCacheList);
+        await queryRunner.commitTransaction();
+
+        await this.worldState.worldStateDB.commit(); // here only 'SYNC_DATA_TREE' commits underlyingly      
+        logger.info(`sync data_tree all done at this round.`);
 
     } catch (err) {
-        throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+        await queryRunner.rollbackTransaction();
+        await this.worldState.worldStateDB.rollback();
+        logger.error(err);
+
+    } finally {
+        await queryRunner.release();
     }
+
+    return {
+        code: 0, data: '', msg: ''
+    };
+
 }
 
 const schema = {
-    description: 'query MerkleTree Info',
+    description: 'sync Lazy Data Tree',
     tags: ['MerkleTree'],
-    params: {
-        type: "object",
-        properties: {
-            blockId: {
-                type: "number",
-            }
-        }
-    },
     response: {
         200: {
             type: 'object',
