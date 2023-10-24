c2d481ab750688ab7c618d62bbeb18d7243b5b6a
coldstar1993 <coldstar1993@163.com>
2023-10-24 08:19:50 +0800

    add more log

--- apps/rollup-sequencer/src/prover.ts
+++ apps/rollup-sequencer/src/prover.ts
@@ -1,14 +1,15 @@
+import { getLogger } from './lib/logUtils';
+
 import { parentPort } from "worker_threads";
 import { WorldStateDB, RollupDB, IndexDB } from "./worldstate";
 import config from "@/lib/config";
 import { activeMinaInstance } from "@anomix/utils";
 import { ProofScheduler } from "./rollup/proof-scheduler";
 import { ProofTaskDto, RollupTaskDto, RollupTaskType, FlowTask, FlowTaskType } from '@anomix/types';
-import { getLogger } from "@/lib/logUtils";
 import { initORM } from "./lib";
 import fs from "fs";
 import { getDateString } from "./lib/timeUtils";
-const logger = getLogger('prover', 'prover');
+const logger = getLogger('prover');
 
 // init Mina tool
 await activeMinaInstance();// TODO improve it to configure graphyQL endpoint
@@ -28,45 +29,49 @@ await initORM();
 const proofScheduler = new ProofScheduler();
 
 process!.on('message', async dto => {
-    if ((dto as any).taskType >= RollupTaskType.ROLLUP_PROCESS) {// TODO MUST improve it！！
-        const rollupTask = dto as RollupTaskDto<any, any>;
+    try {
+        if ((dto as any).taskType >= RollupTaskType.ROLLUP_PROCESS) {// TODO MUST improve it！！
+            const rollupTask = dto as RollupTaskDto<any, any>;
 
-        switch (rollupTask.taskType) {
-            case RollupTaskType.ROLLUP_PROCESS:
-                await proofScheduler.startBatchMerge(rollupTask.payload);
-                break;
+            switch (rollupTask.taskType) {
+                case RollupTaskType.ROLLUP_PROCESS:
+                    await proofScheduler.startBatchMerge(rollupTask.payload);
+                    break;
 
-            case RollupTaskType.ROLLUP_CONTRACT_CALL:
-                await proofScheduler.callRollupContract(rollupTask.payload.blockId);
-                break;
+                case RollupTaskType.ROLLUP_CONTRACT_CALL:
+                    await proofScheduler.callRollupContract(rollupTask.payload.blockId);
+                    break;
 
-            default: // rid it
-                logger.info('rid RollupTask', JSON.stringify(rollupTask));
-                break;
+                default: // rid it
+                    logger.info('rid RollupTask', JSON.stringify(rollupTask));
+                    break;
+            }
+        } else {
+            // ============================== below 'ProofTaskDto' from 'proof-generators' ============================== //
+            // then proof result from 'proof-generators'
+            const proofTaskDto = dto as ProofTaskDto<any, any>;
+            const flowTask = (proofTaskDto.payload) as FlowTask<any>;
+            switch (flowTask.taskType) {
+                case FlowTaskType.ROLLUP_TX_BATCH_MERGE:
+                    fs.writeFileSync(`./ROLLUP_TX_BATCH_MERGE_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
+                    await proofScheduler.whenMergedResultComeBack(proofTaskDto.index.blockId, flowTask.data);
+                    break;
+                case FlowTaskType.BLOCK_PROVE:
+                    fs.writeFileSync(`./BLOCK_PROVE_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
+                    await proofScheduler.whenL2BlockComeback(proofTaskDto.index.blockId, flowTask.data);
+                    break;
+                case FlowTaskType.ROLLUP_CONTRACT_CALL:
+                    fs.writeFileSync(`./ROLLUP_CONTRACT_CALL_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
+                    await proofScheduler.whenL1TxComeback(proofTaskDto.index.blockId, flowTask.data);
+                    break;
+                default: // rid it
+                    logger.info('rid FlowTask', JSON.stringify(flowTask));
+                    break;
+            }
         }
-    } else {
-        // ============================== below 'ProofTaskDto' from 'proof-generators' ============================== //
-        // then proof result from 'proof-generators'
-        const proofTaskDto = dto as ProofTaskDto<any, any>;
-        const flowTask = (proofTaskDto.payload) as FlowTask<any>;
-        switch (flowTask.taskType) {
-            case FlowTaskType.ROLLUP_TX_BATCH_MERGE:
-                fs.writeFileSync(`./ROLLUP_TX_BATCH_MERGE_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
-                await proofScheduler.whenMergedResultComeBack(proofTaskDto.index.blockId, flowTask.data);
-                break;
-            case FlowTaskType.BLOCK_PROVE:
-                fs.writeFileSync(`./BLOCK_PROVE_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
-                await proofScheduler.whenL2BlockComeback(proofTaskDto.index.blockId, flowTask.data);
-                break;
-            case FlowTaskType.ROLLUP_CONTRACT_CALL:
-                fs.writeFileSync(`./ROLLUP_CONTRACT_CALL_proofTaskDto_${proofTaskDto.index?.uuid}_proofResult_${proofTaskDto.index.blockId}_${getDateString()}.json`, JSON.stringify(proofTaskDto));
-                await proofScheduler.whenL1TxComeback(proofTaskDto.index.blockId, flowTask.data);
-                break;
-            default: // rid it
-                logger.info('rid FlowTask', JSON.stringify(flowTask));
-                break;
-        }
+
+    } catch (error) {
+        logger.error(error);
     }
-
     process.send!('done');
 });
