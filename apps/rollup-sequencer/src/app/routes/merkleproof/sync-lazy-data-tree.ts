a3db350d3f53b3227ac5ef0f22a64fd4e94ad37f
coldstar1993 <coldstar1993@163.com>
2023-10-26 16:46:13 +0800

    rebuild merkletree if treeInconsistentFlag

--- apps/rollup-sequencer/src/app/routes/merkleproof/sync-lazy-data-tree.ts
+++ apps/rollup-sequencer/src/app/routes/merkleproof/sync-lazy-data-tree.ts
@@ -1,13 +1,15 @@
 
-import httpCodes from "@inip/http-codes"
 import { FastifyPlugin } from "fastify"
-import { BaseResponse, BlockCacheStatus, BlockCacheType, MerkleTreeId } from '@anomix/types'
+import { BaseResponse, BlockCacheStatus, BlockCacheType, MerkleTreeError, MerkleTreeId } from '@anomix/types'
 import { RequestHandler } from '@/lib/types'
-import { getConnection } from "typeorm"
+import { In, LessThanOrEqual, getConnection } from "typeorm"
 import { Block, BlockCache } from "@anomix/dao"
 import { Field } from "o1js";
 import { getLogger } from "@/lib/logUtils"
-
+import { getDateString } from "@/lib/timeUtils"
+import config from "@/lib/config"
+import { WorldStateDB } from "@/worldstate"
+import fs from "fs";
 const logger = getLogger('syncLazyDataTree');
 
 /**
@@ -31,6 +33,9 @@ export const handler: RequestHandler<null, null> = async function (
     req,
     res
 ): Promise<BaseResponse<string>> {
+    let treeInconsistentFlag = false;
+    let treeIssueAtBlockId = 0;
+
     const connection = getConnection();
     const queryRunner = connection.createQueryRunner();
 
@@ -56,6 +61,8 @@ export const handler: RequestHandler<null, null> = async function (
 
             const block = await queryRunner.manager.findOne(Block, { id: blockCache.blockId });
             logger.info(`block.id: ${block!.id}, block!.dataTreeRoot0: ${block!.dataTreeRoot0}`);
+            // record treeIssueAtBlockId for later handle when issue occur...
+            treeIssueAtBlockId = block!.id;
 
             let syncDataTreeRoot = this.worldState.worldStateDBLazy.getRoot(MerkleTreeId.DATA_TREE, true);
             logger.info(`before sync, syncDataTreeRoot: ${syncDataTreeRoot.toString()}`);
@@ -63,8 +70,9 @@ export const handler: RequestHandler<null, null> = async function (
             logger.info(`before sync, syncDataTreeLeafNum: ${syncDataTreeLeafNum.toString()}`);
 
             if (block!.dataTreeRoot0 != syncDataTreeRoot.toString()) {
+                treeInconsistentFlag = true;
                 logger.error(`block.dataTreeRoot0 is not aligned with current syncDataTreeRoot`);
-                throw new Error(`block.dataTreeRoot0 is not aligned with current syncDataTreeRoot`);
+                throw new MerkleTreeError(MerkleTreeId.DATA_TREE, `block.dataTreeRoot0 is not aligned with current syncDataTreeRoot`);
             }
 
             logger.info(`blockCache.cache: ${blockCache.cache}`);
@@ -78,10 +86,12 @@ export const handler: RequestHandler<null, null> = async function (
             logger.info(`after sync, syncDataTreeLeafNum: ${syncDataTreeLeafNum.toString()}`);
 
             if (block!.dataTreeRoot1 != syncDataTreeRoot.toString()) {
+                treeInconsistentFlag = true;
+
                 logger.info(`block!.dataTreeRoot1: ${block!.dataTreeRoot1}`);
 
                 logger.error(`after sync blockCache, block.dataTreeRoot1 is not aligned with current syncDataTreeRoot`);
-                throw new Error(`after sync blockCache, block.dataTreeRoot1 is not aligned with current syncDataTreeRoot`);
+                throw new MerkleTreeError(MerkleTreeId.DATA_TREE, `after sync blockCache, block.dataTreeRoot1 is not aligned with current syncDataTreeRoot`);
             }
 
             logger.info(`process blockCache.id = ${blockCache.id} done.`);
@@ -109,6 +119,76 @@ export const handler: RequestHandler<null, null> = async function (
 
     } finally {
         await queryRunner.release();
+    }
+
+    if (treeInconsistentFlag && treeIssueAtBlockId > 1) {
+        logger.info(`start rebuilding WorldStateDBLazy...`);
+
+        const currentDateString = getDateString();
+        const rebuildWorldStateDBLazyPath = config.worldStateDBLazyPath.concat(`-block-${treeIssueAtBlockId - 1}-rebuilding-${currentDateString}`);
+        const rebuildWorldStateDBLazy = new WorldStateDB(rebuildWorldStateDBLazyPath);
+
+        try {
+            const treeId = MerkleTreeId.DATA_TREE;
+            // if merkletree error, then new trees instantly and switch to the new ones!
+            const blockCacheList = await getConnection().getRepository(BlockCache)
+                .find({
+                    where: {
+                        type: BlockCacheType.DATA_TREE_UPDATES,
+                        blockId: LessThanOrEqual(treeIssueAtBlockId - 1) // till 'treeIssueAtBlockId - 1'
+                    },
+                    order: { blockId: 'ASC' }
+                });
+
+            logger.info(`start rebuild ${MerkleTreeId[treeId]} ...`);
+
+            for (let i = 0; i < blockCacheList.length; i++) {
+                const blockCache = blockCacheList[i];
+                logger.info(`  processing blockCache[${blockCache.id}] at Block[${blockCache.blockId}]...`);
+                await rebuildWorldStateDBLazy.appendLeaves(treeId, (JSON.parse(blockCache.cache) as string[]).map(c => Field(c)));
+
+                const existingCurrentBlock = await getConnection().getRepository(Block).findOne({ where: { id: blockCache.blockId } });
+                const newRoot = rebuildWorldStateDBLazy.getRoot(treeId, true).toString();
+                logger.info(`  new ${MerkleTreeId[treeId]} root: ${newRoot}`);
+                logger.info(`  existingCurrentBlock.dataTreeRoot1: ${existingCurrentBlock!.dataTreeRoot1}`);
+
+                if (newRoot != existingCurrentBlock!.dataTreeRoot1) {
+                    logger.error(`  rebuild ${MerkleTreeId[treeId]} failed, due to two roots are not equal.`);
+                    throw new Error(`rebuild ${MerkleTreeId[treeId]} failed, due to two roots are not equal.`);
+                }
+            }
+            logger.info(`  append all Leaves done.`);
+
+            logger.info(`rebuildWorldStateDBLazy.commit...`);
+            await rebuildWorldStateDBLazy.commit();
+
+            logger.info(`rebuildWorldStateDBLazy close...`);
+            await rebuildWorldStateDBLazy.close();
+            logger.info(`original worldStateDBLazy close...`);
+            await this.worldState.worldStateDBLazy.close();
+
+            const migrateDir = config.worldStateDBLazyPath.concat(`-block-${treeIssueAtBlockId - 1}-broken-${currentDateString}`);
+            logger.info(`rename dir of original worldStateDBLazy to ${migrateDir}...`);
+            fs.renameSync(config.worldStateDBLazyPath, migrateDir);
+            logger.info(`rename dir of rebuildWorldStateDBLazy to ${config.worldStateDBLazyPath}...`);
+            fs.renameSync(rebuildWorldStateDBLazyPath, config.worldStateDBLazyPath);
+
+            logger.info(`re-open new WorldStateDB...`);
+            await rebuildWorldStateDBLazy.open();
+
+            logger.info(`change pointer to new WorldStateDB...`);
+            this.worldState.worldStateDBLazy = rebuildWorldStateDBLazy;
+
+            logger.info(`rebuild ${MerkleTreeId[treeId]} done.`);
+
+            logger.info(`rebuild WorldStateDBLazy end.`);
+        } catch (err) {
+            logger.error(err);
+
+            await rebuildWorldStateDBLazy.rollback();
+
+            throw err;
+        }
     }
 
     return {
