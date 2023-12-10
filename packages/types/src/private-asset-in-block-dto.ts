50cf7266cfd8eba27aad07c8dcd99f1f1d5d9c0e
coldstar1993 <coldstar1993@163.com>
2023-12-10 21:11:53 +0800

    define L2TxSimpleDto interface

--- packages/types/src/private-asset-in-block-dto.ts
+++ packages/types/src/private-asset-in-block-dto.ts
@@ -27,3 +27,74 @@ export interface AssetsInBlockDto {
      */
     finalizedTs: number
 }
+
+/**
+ * L2 tx dto for simplifying the L2 tx to return to frontend
+ */
+export interface L2TxSimpleDto {
+    id: number
+
+
+    txHash: string
+
+
+    actionType: string
+
+
+    nullifier1: string
+
+    nullifierIdx1?: string
+
+
+    nullifier2: string
+
+    nullifierIdx2?: string
+
+
+    outputNoteCommitment1: string
+    /**
+     * leaf index on data_tree
+     */
+    outputNoteCommitmentIdx1?: string
+
+
+    outputNoteCommitment2: string
+    /**
+     * leaf index on data_tree
+     */
+    outputNoteCommitmentIdx2?: string
+
+
+    publicValue: string
+
+
+    publicOwner: string
+
+
+    publicAssetId: string
+
+
+    dataRoot: string
+
+    /**
+     * for deposit L2tx
+     */
+    depositRoot: string
+
+    /**
+     * leaf index of `outputNoteCommitment1` on deposit_tree
+     */
+    depositIndex: string
+
+
+    txFee: string
+
+
+    txFeeAssetId: string
+
+    /**
+     * the index within a block
+     */
+    indexInBlock?: number
+
+}
