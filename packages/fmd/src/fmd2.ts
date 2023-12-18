4ce803046e6ce7bd7701014f09009a57f5b6005c
Comdex <wcomdex@foxmail.com>
2023-12-18 22:07:05 +0800

    Implement relevant hash methods of fmd

--- packages/fmd/src/fmd2.ts
+++ packages/fmd/src/fmd2.ts
@@ -1,12 +1,12 @@
-import { ProjPointType } from "@noble/curves/abstract/weierstrass";
 import { p256, secp256r1 } from "@noble/curves/p256";
 import * as utils from "@noble/curves/abstract/utils";
+import { sha256 } from "@noble/hashes/sha256";
 
 let priKey = p256.utils.randomPrivateKey();
 let pubKey = p256.getPublicKey(priKey);
 
 const Point = p256.ProjectivePoint;
-type Point = Point;
+type Point = typeof Point.BASE;
 
 class PublicKey {
     numKeys: number;
@@ -82,10 +82,9 @@ function flag(pk: PublicKey): Flag {
     let u = Point.BASE.multiply(r);
     let z = p256.utils.normPrivateKeyToScalar(p256.utils.randomPrivateKey());
     let w = Point.BASE.multiply(z);
-    Point.fromBytes();
 
-    let c = [];
-    let k = [];
+    let c: number[] = [];
+    let k: number[] = [];
 
     for (let i = 0; i < 15; i++) {
         let x = Point.fromHex(utils.bytesToHex(pubKey[i])).x;
@@ -97,6 +96,52 @@ function flag(pk: PublicKey): Flag {
     m = hash_g(u.x, u.y, c);
 }
 
-function hash_h(u: Point, h: Point, w: Point) {}
+function encrypt_string(hash_string: string) {
+    return sha256(hash_string);
+}
 
-function hash_g(ux: number, uy: number, c: number[]) {}
+function hash_h(u: Point, h: Point, w: Point) {
+    let str =
+        u.x.toString() +
+        u.y.toString() +
+        h.x.toString() +
+        h.y.toString() +
+        w.x.toString() +
+        w.y.toString();
+    return encrypt_string(str)[0] & 0x01;
+}
+
+function concatenateUint8Arrays(a: Uint8Array, b: Uint8Array): Uint8Array {
+    const result: Uint8Array = new Uint8Array(a.length + b.length);
+    result.set(a, 0);
+    result.set(b, a.length);
+    return result;
+}
+
+function uint8ArrayToBigInt(bytes: Uint8Array): bigint {
+    return bytes.reduce((acc, curr) => (acc << 8n) + BigInt(curr), 0n);
+}
+
+function hash_g(ux: bigint, uy: bigint, c: number[]) {
+    let str = ux.toString() + uy.toString();
+    for (let i = 0; i < c.length; i++) {
+        str += c[i].toString();
+    }
+
+    let size = p256.CURVE.nBitLength + 64;
+    let result = new Uint8Array();
+    let bit_hashed = 0;
+
+    while (bit_hashed < size) {
+        let hashed = encrypt_string(str);
+        result = concatenateUint8Arrays(result, hashed.slice(0, 32));
+        bit_hashed = result.length / 8;
+
+        if (bit_hashed < size) {
+            str += "x";
+        }
+    }
+
+    let res = uint8ArrayToBigInt(result) % p256.CURVE.n;
+    return res;
+}
