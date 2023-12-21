a4a720c91a9c91d33646cf960088e9608cec4fd1
coldstar1993 <coldstar1993@163.com>
2023-12-21 23:32:40 +0800

    add start-new-flow endpoint for renew the state

---
+++ apps/rollup-sequencer/src/app/routes/trigger-seq-commitment.ts
@@ -0,0 +1,57 @@
+
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { RequestHandler } from '@/lib/types'
+import { BaseResponse } from "@anomix/types";
+
+/**
+ * trigger triggerStartNewFlow for sequence commitment
+ */
+export const triggerStartNewFlow: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "GET",
+        url: "/rollup/start-new-flow",
+        //preHandler: [instance.authGuard],
+        schema,
+        handler
+    })
+}
+
+export const handler: RequestHandler<null, null> = async function (
+    req,
+    res
+): Promise<BaseResponse<boolean>> {
+    try {
+        // start a new flow! 
+        await this.worldState.startNewFlow();
+
+        return { code: 0, data: true, msg: '' };
+    } catch (err) {
+        throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
+
+const schema = {
+    description: 'trigger deposit actions rollup',
+    tags: ['Rollup'],
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: 'number'
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
