4f4dd2392a14e251193a4fef4da6021e1575f73d
coldstar1993 <coldstar1993@163.com>
2023-12-26 23:21:12 +0800

    initially add endpoint queryBlockByBlockHeight

--- apps/open-api/src/app/routes/block/query-by-height.ts
+++ apps/open-api/src/app/routes/block/query-by-height.ts
@@ -1,1 +1,71 @@
-//@Get('block/height/:height')
+
+import httpCodes from "@inip/http-codes"
+import { FastifyPlugin } from "fastify"
+import { RequestHandler } from '@/lib/types'
+import { BaseResponse, BlockDto } from "@anomix/types";
+import { Block } from "@anomix/dao";
+import { getConnection } from 'typeorm';
+import { getLogger } from "@/lib/logUtils";
+
+const logger = getLogger('queryLatestBlockHeight');
+
+/**
+ * query the latest block-height
+ */
+export const queryLatestBlockHeight: FastifyPlugin = async function (
+    instance,
+    options,
+    done
+): Promise<void> {
+    instance.route({
+        method: "GET",
+        url: "/block/:blockHeight",
+        //preHandler: [instance.authGuard],
+        schema,
+        handler
+    })
+}
+
+export const handler: RequestHandler<null, { blockHeight: number }> = async function (
+    req,
+    res
+): Promise<BaseResponse<BlockDto>> {
+    try {
+        const connection = getConnection();
+
+        const blockRepository = connection.getRepository(Block);
+        // query latest block
+        const blockEntity = (await blockRepository.findOne({
+            id: req.params.blockHeight
+        }));
+
+
+        return { code: 0, data: blockEntity, msg: '' };
+    } catch (err) {
+        logger.error(err);
+        console.error(err);
+
+        throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
+
+const schema = {
+    description: 'query the latest block-height',
+    tags: ['Block'],
+    response: {
+        200: {
+            type: 'object',
+            properties: {
+                code: {
+                    type: 'number',
+                },
+                data: {
+                    type: 'object',
+                },
+                msg: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
---
+++ packages/types/src/block-dto.ts
@@ -0,0 +1,94 @@
+export interface BlockDto {
+    /**
+     * i.e. blockHeight, should starts from 1
+     */
+
+    id: number
+
+    /**
+     * the hash of the block
+     */
+
+    blockHash: string
+
+
+    rollupSize: number;
+
+
+
+    rootTreeRoot0: string
+
+
+    dataTreeRoot0: string
+
+
+    nullifierTreeRoot0: string
+
+
+    depositStartIndex0: string
+
+
+    rootTreeRoot1: string
+
+
+    dataTreeRoot1: string
+
+    /**
+     * record each dataTreeRoot's Index on rootTree
+     */
+
+    dataTreeRoot1Indx: string
+
+
+    nullifierTreeRoot1: string
+
+
+    depositStartIndex1: string
+
+
+
+    depositRoot: string
+
+
+    depositCount: number
+
+    /**
+     * json string from: Provable.Array(TxFee, FEE_ASSET_ID_SUPPORT_NUM)
+     */
+
+    totalTxFees: string
+
+
+    txFeeReceiver: string
+
+
+    txFeeCommitment: string
+
+    /**
+     * record the L1TxHash when it's broadcast
+     */
+
+    l1TxHash: string
+
+
+    status: number
+
+    /**
+     * the timestamp when L2Block is finalized at Layer1
+     */
+
+    finalizedAt: Date
+
+    /**
+     * record timestamp for each proof-gen trigger
+     * * used at coordinator_proof-trigger to avoid repeatedly trigger proof-gen at a short time-window.
+     */
+
+    triggerProofAt: Date
+
+
+    updatedAt: Date
+
+
+    createdAt: Date
+}
