89d88ff25cce2dc15de23a4528cf45a9c218edf1
coldstar1993 <coldstar1993@163.com>
2024-01-27 10:35:22 +0800

    Processing Proof Notification

---
+++ apps/coordinator/src/grpc/server/server.ts
@@ -0,0 +1,24 @@
+import grpc from "@grpc/grpc-js";
+import protoLoader from "@grpc/proto-loader";
+import { queryTxByNullifierEndpoint, withdrawAssetEndpoint } from "./handlers/coordinatorHandler";
+
+const PROTO_PATH = __dirname + './protos/coordinator.proto';
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+});
+
+const coordinator = grpc.loadPackageDefinition(packageDefinition).coordinator;
+
+function getGrpcServer() {
+    const server = new grpc.Server();
+    server.addService(coordinator.Coordinator.service, {
+        highFeeTxExist,
+        proofNotify,
+    });
+    return server;
+}
