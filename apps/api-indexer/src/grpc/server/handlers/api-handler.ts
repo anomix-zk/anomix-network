2c8a4dcbfeff7e50a483e81bd21aaf2a9b83a8c6
coldstar1993 <coldstar1993@163.com>
2024-02-18 16:48:42 +0800

    add checkAliasRegister

--- apps/api-indexer/src/grpc/server/handlers/api-handler.ts
+++ apps/api-indexer/src/grpc/server/handlers/api-handler.ts
@@ -120,3 +120,52 @@ export async function checkAliasAlignWithViewKey(dto: { aliasHash: string, acctV
     }
 }
 
+export async function checkAliasRegister(dto: { aliashash: string, includePending: boolean }) {
+
+    const { aliashash: p_aliashash, includePending } = dto
+
+    const connection = getConnection();
+    const accountRepository = connection.getRepository(Account)
+    try {
+        let code = 0;// registered!
+        let data = true;
+        let msg = '';
+        const account = await accountRepository.findOne({ where: { aliasHash: p_aliashash } });
+        if (account /* && includePending */) {
+            const mpL2TxRepo = connection.getRepository(MemPlL2Tx);
+            const mpL2Tx = await mpL2TxRepo.findOne(account.l2TxHash);
+            if (!mpL2Tx) {
+                const l2TxRepo = connection.getRepository(L2Tx);
+                const l2Tx = await l2TxRepo.findOne(account.l2TxHash);
+                if (!l2Tx) {
+                    code = 1;// not registered!
+                    data = false;
+                }
+
+            } else {
+                const status = (mpL2Tx)!.status;
+                if (status == L2TxStatus.FAILED) {
+                    code = 1;// not registered!
+                    data = false;
+                } else if (status == L2TxStatus.PENDING) {
+                    msg = 'pending';
+                }
+            }
+
+        } else {
+            data = false;
+            msg = 'no this alias!'
+        }
+
+        return {
+            code,
+            data,
+            msg
+        };
+
+    } catch (err) {
+        logger.error(err);
+
+        // throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
