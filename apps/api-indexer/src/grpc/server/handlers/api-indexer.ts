88408a8779697e4826f74b50085b194c6b7d4d29
coldstar1993 <coldstar1993@163.com>
2024-02-24 16:58:46 +0800

    add queryWithdrawalNotesx handler

--- apps/api-indexer/src/grpc/server/handlers/api-indexer.ts
+++ apps/api-indexer/src/grpc/server/handlers/api-indexer.ts
@@ -755,3 +755,87 @@ export async function queryTxByNullifier(dto: string[]) {
         // throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
     }
 }
+
+export async function queryWithdrawalNotesx(dto: { commitments: string[], l1addr: string }) {
+
+    const noteCommitmentList = dto.commitments;
+    const l1addr = dto.l1addr;
+
+    if (!(l1addr || noteCommitmentList?.length > 0)) {
+        return {
+            code: 1,
+            data: [],
+            msg: 'both l1addr and commitments are none!'
+        };
+    }
+
+    try {
+        const connection = getConnection();
+        const withdrawInfoRepository = connection.getRepository(WithdrawInfo);
+
+        /* ====================== NEED PERFORMANCE IMPROVEMENT LATER ======================*/
+        const queryBuilder = withdrawInfoRepository.createQueryBuilder('wi').andWhere(`wi.assetId = '${1}'`)
+        if (l1addr) {
+            queryBuilder.andWhere(`wi.ownerPk = '${l1addr}'`)
+        }
+        if (noteCommitmentList?.length > 0) {
+            queryBuilder.andWhere(`wi.outputNoteCommitment in (${noteCommitmentList.map(c => `'${c}'`).join(',')})`);
+        }
+        let withdrawInfoList = (await queryBuilder.getMany()) ?? [];
+
+        if (noteCommitmentList?.length == 1 && withdrawInfoList.length == 1) {// when the commitment is existing in db
+            if (withdrawInfoList[0].status == WithdrawNoteStatus.DONE) {
+                return {
+                    code: 1,
+                    data: [],
+                    msg: 'The note has been claimed already!'
+                };
+            }
+        }
+
+        // exclude the DONE ones.
+        withdrawInfoList = withdrawInfoList.filter(w => w.status != WithdrawNoteStatus.DONE);
+
+        const blockRepository = connection.getRepository(Block);
+        const txRepository = connection.getRepository(L2Tx);
+        const withdrawInfoDtoList: WithdrawInfoDto[] = [];
+        for (let i = 0; i < withdrawInfoList.length; i++) {
+            const wInfo = withdrawInfoList[i];
+            const { createdAt, updatedAt, ...restObj } = wInfo;
+            const withdrawInfoDto = (restObj as any) as WithdrawInfoDto;
+
+            let block: Block | undefined = undefined as any;
+            if (!(withdrawInfoDto.l2TxHash)) {// for querying operator txFee
+                block = await blockRepository.findOne({ where: { txFeeCommitment: wInfo.outputNoteCommitment, status: BlockStatus.CONFIRMED } });
+            } else {
+                // then query confirmed tx collection
+                const tx = await txRepository.findOne({
+                    where: {
+                        txHash: withdrawInfoDto.l2TxHash
+                    }
+                });
+
+                block = await blockRepository.findOne({ where: { id: tx!.blockId, status: BlockStatus.CONFIRMED } });
+            }
+
+            if (block) {
+                withdrawInfoDto.createdTs = block!.createdAt.getTime();
+                withdrawInfoDto.finalizedTs = block!.finalizedAt.getTime();
+                withdrawInfoDtoList.push(withdrawInfoDto);
+            }
+        }
+
+        /* ====================== NEED PERFORMANCE IMPROVEMENT LATER ======================*/
+
+        return {
+            code: 0,
+            data: withdrawInfoDtoList,
+            msg: ''
+        };
+
+    } catch (err) {
+        logger.error(err);
+        console.error(err);
+        // throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
