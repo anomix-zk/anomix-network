39129ea5e5807353889c707c361059ad3519ebdd
coldstar1993 <coldstar1993@163.com>
2024-02-25 21:20:02 +0800

    add withdrawAsset handler

--- apps/api-sequencer/src/grpc/server/handlers/api-sequencer.ts
+++ apps/api-sequencer/src/grpc/server/handlers/api-sequencer.ts
@@ -20,3 +20,59 @@ import fs from "fs";
 
 const logger = getLogger('api-sequencer');
 
+
+/**
+ * withdrawAsset
+ * @param dto 
+ */
+export async function withdrawAsset(dto: WithdrawAssetReqDto) {
+
+    const { l1addr, noteCommitment } = dto
+
+    const withdrawInfoRepository = getConnection().getRepository(WithdrawInfo)
+    try {
+        const previousWithdrawInfoList = await withdrawInfoRepository.find({
+            where: {
+                ownerPk: l1addr,
+                assetId: '1',// default Mina
+            }
+        }) ?? [];
+
+        if (previousWithdrawInfoList.length == 0) {
+            return {
+                code: 1,
+                data: '',
+                msg: 'no PENDING withdraw info found!'
+            };
+        }
+
+        const processingOne = previousWithdrawInfoList.filter(wi => {
+            return wi.status == WithdrawNoteStatus.PROCESSING;
+        })[0];
+        // due to need update USER_NULLIFIER_TREE root at user's tokenAccount<L1Addr, AssetID>, withdrawal should be in sequence.
+        if (processingOne.outputNoteCommitment == noteCommitment) {
+            return {
+                code: 1,
+                data: '',
+                msg: `Please wait for last withdraw of <${l1addr}, assetId:${processingOne!.assetId}, assetNote: ${noteCommitment}> to be done!`
+            };
+        }
+
+        const targetOne = previousWithdrawInfoList.filter(wi => {
+            return wi.outputNoteCommitment == noteCommitment;
+        })[0];
+        if (!(targetOne?.status == WithdrawNoteStatus.PENDING)) {
+            return {
+                code: 1,
+                data: '',
+                msg: 'no PENDING withdraw info found!'
+            };
+        }
+
+        //send to 'Sequencer' for further handle.
+        return await $axiosSeq.post<BaseResponse<string>>('/tx/withdraw', { l1addr, noteCommitment }).then(r => r.data);
+
+    } catch (err) {
+        // throw req.throwError(httpCodes.INTERNAL_SERVER_ERROR, "Internal server error")
+    }
+}
